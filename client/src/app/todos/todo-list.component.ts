8b0e4dc3211e77cb3689f013917f96c9979de51c
JoshuaHamann <joshwashku@yahoo.com>
2020-03-01 14:25:29 -0600

    Create shells for todo-list
    Now our app.module is not mad anymore.

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -2,3 +2,21 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Todo } from './todo';
 import { TodoService } from './todo.service';
 import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-todo-list-component',
+  templateUrl: 'todo-list.component.html',
+  providers: [],
+})
+
+export class TodoListComponent implements OnInit, OnDestroy {
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
+
+}
