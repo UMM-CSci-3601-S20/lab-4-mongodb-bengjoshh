843fd381c02e6f936ab4f7e48d9797a4a86d3673
JoshuaHamann <joshwashku@yahoo.com>
2020-03-01 14:00:00 -0600

    Add TodoService in app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { UserCardComponent } from './users/user-card.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { AddUserComponent } from './users/add-user.component';
+import { TodoService } from './todos/todo.service';
 
 const MATERIAL_MODULES: any[] = [
   MatListModule,
@@ -60,6 +61,7 @@ const MATERIAL_MODULES: any[] = [
     UserCardComponent,
     UserProfileComponent,
     AddUserComponent,
+    TodoService,
   ],
   imports: [
     BrowserModule,
@@ -73,7 +75,8 @@ const MATERIAL_MODULES: any[] = [
     LayoutModule,
   ],
   providers: [
-    UserService
+    UserService,
+    TodoService
   ],
   bootstrap: [AppComponent]
 })
